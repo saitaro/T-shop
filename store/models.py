84c4c559549697e24d39bda283cf76bd469760c2
Vici <ripsodi@gmail.com>
2019-07-10 01:54:38 +0300

    Add instructions to readme

--- store/models.py
+++ store/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 from django.db import models
-from django.db.models.fields import CharField
 from django.db.models import Sum, F, DecimalField
 
 from phonenumber_field.modelfields import PhoneNumberField
