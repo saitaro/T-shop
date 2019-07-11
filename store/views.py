5a6e1fa298174d23d3e6bd20b9932915e53ec9dd
saitaro <nort87@mail.ru>
2019-07-11 01:43:08 +0000

    Fix filtering bug

--- store/views.py
+++ store/views.py
@@ -8,16 +8,26 @@ from .decorators import force_http, force_https
 # @force_http
 def catalog(request):
     if request.GET:
-        categories = CategoryForm(request.GET)
-        choices = dict(categories.data)['categories']
+        form = CategoryForm(request.GET)
+        form.is_valid()
+        choices = form.cleaned_data['categories']
+        request.session['categories'] = choices
         products = Product.objects.filter(category__id__in=choices)
     else:
-        categories = CategoryForm()
-        products = Product.objects.all()
+        if request.session.get('add') and request.session.get('categories'):
+            choices = request.session['categories']
+            form = CategoryForm({'categories': choices})
+            products = Product.objects.filter(category__id__in=choices)
+        else:
+            if request.session.get('categories'):
+                request.session['categories'].clear()
+            form = CategoryForm()
+            products = Product.objects.all()
+        request.session['add'] = False
 
     context = {
         'products': products,
-        'categories': categories,
+        'form': form,
         'cart': sum(request.session.get('cart', {}).values()),
     }
     return render(request, 'catalog.html', context)
@@ -28,6 +38,7 @@ def add_to_cart(request, pk):
     cart = request.session.get('cart', {})
     cart[pk] = cart.get(pk, 0) + 1
     request.session['cart'] = cart
-    print(cart)
+    request.session['add'] = True
     return redirect('store:catalog')
 
+    
